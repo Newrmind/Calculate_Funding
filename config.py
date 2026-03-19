f620b96c1f950da4287da7bfaa49f9f6b93e8d19
albert <albert9171@gmail.com>
2026-03-19 12:31:22 +0300

    Фиксинг 19.03.26.

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
 
     futures_params = {"USDRUBF": ["USD_RUB", 0.001, 0.0015], "EURRUBF": ["EUR_RUB", 0.001, 0.0015]}  # {"Тикер": ["Тикер_спота", K1, K2]}
 
-    CNY_RUB_FIXME = 11.31
+    CNY_RUB_FIXME = 12.35
     USD_CNY_FIXME = None
 
-    EXPIRE_DATE = {'Y': 2025, 'M': 12, 'D': 18}
+    EXPIRE_DATE = {'Y': 2026, 'M': 6, 'D': 18} # Установлена дата для фьючей 06.26
